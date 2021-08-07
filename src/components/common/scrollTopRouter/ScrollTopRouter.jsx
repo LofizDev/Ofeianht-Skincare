13950eab4fe09fc61a6320dab60472345957a1c7
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-08-07 23:21:59 +0700

    fixing location

--- src/components/common/scrollTopRouter/ScrollTopRouter.jsx
+++ src/components/common/scrollTopRouter/ScrollTopRouter.jsx
@@ -2,9 +2,9 @@ import {useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
 
 export default function ScrollToTopRouter() {
-  const {pathname} = useLocation();
+  // const {pathname} = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, []);
   return null;
 }
