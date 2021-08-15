cc0f5b0ad42d2f4d6460a3f76af4357f505ef986
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-08-15 23:27:27 +0700

    changes route testing

--- src/components/Layout/UrlNames.jsx
+++ src/components/Layout/UrlNames.jsx
@@ -1,7 +1,7 @@
 export const UrlNames = {
-    HOME: '/Ofeianht-Skincare/',
-    SHOP: '/Ofeianht-Skincare/SHOP',
-    CONTACT: '/Ofeianht-Skincare/CONTACT',
-    ABOUT: '/Ofeianht-Skincare/ABOUT',
-    BLOG: '/Ofeianht-Skincare/BLOG',
+    HOME: '/',
+    SHOP: '/SHOP',
+    CONTACT: '/CONTACT',
+    ABOUT: '/ABOUT',
+    BLOG: '/BLOG',
 }
